857d37df67bc9ef2034094eecbd912d42823d965
unknown <bas-jacobs@hotmail.com>
2020-02-14 14:20:23 +0100

    Footer and not found page update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /**
  * Site footer component
@@ -7,12 +7,97 @@ const Footer = () => (
   <footer>
     <p>
       Proudly published with &nbsp;
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">Prismic</a>
+      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
+        Prismic
+      </a>
       <br />
       <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        <img className="footer-logo" src="/images/logo-prismic.svg" alt="Gray Prismic logo" />
+        <img
+          className="footer-logo"
+          src="/images/logo-prismic.svg"
+          alt="Gray Prismic logo"
+        />
       </a>
     </p>
+    <div class="container bottom-footer row">
+      <div class="logo-footer-wrapper small-order-4 medium-order-1 column small-12 medium-3">
+        <span>All rights reserved</span>
+      </div>
+
+      <div>
+        <span class="title">Developers</span>
+        <ul>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <span class="title">Company</span>
+        <ul>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <span class="title">Product</span>
+        <ul>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+          <li>
+            <a href="#">lorem ipsum</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </footer>
 );
 
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 /**
  * Page not found (404) component
  */
+
 const NotFound = () => (
   <div className="not-found">
-    <h1>404</h1>
-    <h2>Document not found</h2>
-    <p><Link to="/">Return to homepage</Link></p>
+    <h2>
+      <strong>404.</strong>That's and error.
+    </h2>
+    <h3>The requested URL ({window.location.pathname}) was not found.</h3>
+    <p>
+      <Link to="/">Return to homepage</Link>
+    </p>
   </div>
 );
 
